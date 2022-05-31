1f9a475db7899299f7a686082bf259b5e1bbae3a
MarcosPontes136 <marcos.ponts@hotmail.com>
2022-05-30 22:03:11 -0300

    Update Project

--- backend/envioEmail/src/main/java/com/ApiEmail/envioEmail/Service/EmailService.java
+++ backend/envioEmail/src/main/java/com/ApiEmail/envioEmail/Service/EmailService.java
@@ -23,19 +23,19 @@ public class EmailService {
 
     public EmailModel sendEmail(EmailModel emailModel) {
     	emailModel.setSendDateEmail(LocalDateTime.now());
-    	try {
-    		SimpleMailMessage message = new SimpleMailMessage();
-    		message.setFrom(emailModel.getEmailFrom());
-            message.setTo(emailModel.getEmailTo());
-            message.setSubject(emailModel.getSubject());
-            message.setText(emailModel.getText());
-            emailSender.send(message);
-    		
-            emailModel.setStatusEmail(StatusEmail.SENT);
-		} catch (MailException e) {
-			emailModel.setStatusEmail(StatusEmail.ERROR);
-		} finally {
-			return emailRepository.save(emailModel);
-		}
+    	 try{
+             SimpleMailMessage message = new SimpleMailMessage();
+             message.setFrom(emailModel.getEmailFrom());
+             message.setTo(emailModel.getEmailTo());
+             message.setSubject(emailModel.getSubject());
+             message.setText(emailModel.getText());
+             emailSender.send(message);
+
+             emailModel.setStatusEmail(StatusEmail.SENT);
+         } catch (MailException e){
+             emailModel.setStatusEmail(StatusEmail.ERROR);
+         } finally {
+             return emailRepository.save(emailModel);
+         }
     }
 }
